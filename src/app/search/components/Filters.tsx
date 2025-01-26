5d07f1d0e4e2baa8b854fd568d8fb062a601c531
Maryam <maryam.bahangeh@gmail.com>
2025-01-26 19:45:10 +0100

    [UPDATE] fix errors of ItemsFilter.tsx

--- src/app/search/components/Filters.tsx
+++ src/app/search/components/Filters.tsx
@@ -10,9 +10,7 @@ import { SPECIALITY_OPTIONS } from "@/options/speciality-options";
 
 import styles from "./Filters.module.css";
 
-import ItemsFilter, {
-  FILTER_VARIANT,
-} from "@/app/search/components/items-filters/ItemsFilter";
+import ItemsFilter from "@/app/search/components/items-filters/ItemsFilter";
 
 import { DEGREE } from "@/options/degree-options";
 import { SERVICE_TYPE } from "@/options/service-types-options";
@@ -25,20 +23,17 @@ function Filters() {
       {Object.keys(filters).length > 0 && <SelectedFilters />}
 
       <ItemsFilter
-        filterVariant={FILTER_VARIANT.LIST}
         title="Speciality"
         options={SPECIALITY_OPTIONS}
         fieldName="specialityName"
       />
       <ItemsFilter
-        filterVariant={FILTER_VARIANT.LIST}
         title="Academic Degree"
         options={DEGREE}
         fieldName="degreeName"
       />
 
       <ItemsFilter
-        filterVariant={FILTER_VARIANT.RADIO}
         title="Service type"
         options={SERVICE_TYPE}
         fieldName="serviceTypeName"
--- src/app/search/components/items-filters/ItemsFilter.tsx
+++ src/app/search/components/items-filters/ItemsFilter.tsx
@@ -7,64 +7,36 @@ import { SelectOptionType } from "@/types/select-option-type";
 
 import styles from "./ItemsFilter.module.css";
 
-export enum FILTER_VARIANT {
-  LIST = "list",
-  RADIO = "radio",
-}
-
 type Props = {
-  filterVariant: FILTER_VARIANT;
   title: string;
   options: SelectOptionType[];
   fieldName: keyof FiltersType;
 };
 
-function ItemsFilter({ filterVariant, title, options, fieldName }: Props) {
+function ItemsFilter({ title, options, fieldName }: Props) {
   const { filters, dispatchFilters } = useContext(FiltersContext);
 
   return (
     <Card title={title} className={styles["items-filter"]}>
       <ul>
-        {options.map(
-          (option) =>
-            (filterVariant === FILTER_VARIANT.LIST && (
-              <button
-                key={option.label}
-                value={option.label}
-                className={
-                  filters[fieldName] === option.label ? styles.selected : ""
-                }
-                onClick={() =>
-                  dispatchFilters({
-                    type: "filtered",
-                    key: fieldName,
-                    value: option.label,
-                  })
-                }
-              >
-                {option.label}
-              </button>
-            )) ||
-            (filterVariant === FILTER_VARIANT.RADIO && (
-              <div key={option.value} className={styles["radio-list"]}>
-                <input
-                  type="radio"
-                  id={option.label}
-                  name={option.label}
-                  value={option.value}
-                  checked={filters[fieldName] === option.label}
-                  onChange={() =>
-                    dispatchFilters({
-                      type: "filtered",
-                      key: fieldName,
-                      value: option.label,
-                    })
-                  }
-                />
-                <label htmlFor={option.label}>{option.label}</label>
-              </div>
-            )),
-        )}
+        {options.map((option) => (
+          <button
+            key={option.label}
+            value={option.label}
+            className={
+              filters[fieldName] === option.label ? styles.selected : ""
+            }
+            onClick={() =>
+              dispatchFilters({
+                type: "filtered",
+                key: fieldName,
+                value: option.label,
+              })
+            }
+          >
+            {option.label}
+          </button>
+        ))}
       </ul>
     </Card>
   );
